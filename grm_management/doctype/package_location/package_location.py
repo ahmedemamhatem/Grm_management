a5c9c900e58bc6bc68ec481ffc5253ab65084f96
waelelsafty07 <waelelsafty07@gmail.com>
2026-01-19 12:31:28 +0200

    Edit modules and rearrange files and add workspace for my doctype

--- grm_management/doctype/package_location/package_location.py
+++
@@ -1,5 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-class PackageLocation(Document):
-    pass
--- grm_management/doctype/package_space_type/package_space_type.py
+++
@@ -1,5 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-class PackageSpaceType(Document):
-    pass
